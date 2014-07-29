9d32b32fd8b980529320adff0b80847947189b1d
Yu He <yu.he@inconspicuous.no>
2014-07-29 11:26:50 +0200

    Removed test code.

--- Example/01 - Test/Context/TestContext.cs
+++ Example/01 - Test/Context/TestContext.cs
@@ -1,29 +1,13 @@
-using System;
-using System.ComponentModel.Composition;
 using DryIoc;
 using UniRx;
 
 namespace Inconspicuous.Framework.Example.Test {
-	public interface IService {
-		void Execute();
-	}
-
-	[Export(typeof(IService))]
-	public class Service : IService {
-		public void Execute() {
-			UnityEngine.Debug.Log("Test");
-		}
-	}
-
 	[Scene("Test")]
 	public class TestContext : Context {
 		public TestContext(IContextView contextView, params Context[] subContexts)
 			: base(contextView, subContexts) {
-			container.RegisterDelegate<Lazy<IService>>(registry => new Lazy<IService>(() => registry.Resolve<IService>()));
 			//container.Register<ICommandHandler<TestCommand, NullResult>,
 			//	DimScreenEffectCommandHandlerDecorator<TestCommand, NullResult>>(setup: DecoratorSetup.With());
-			//var service = container.Resolve<Lazy<IService>>();
-			container.Resolve<Lazy<IService>>().Value.Execute();
 		}
 
 		public override void Start() {
